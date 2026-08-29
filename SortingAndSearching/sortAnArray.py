de1ac4194e4ca66e68854a0a6b6add71ac6f5e55
Prashantpd7 <prashantdwivedipd7@gmail.com>
2026-08-29 14:05:37 +0530

    sortanarray

---
+++ SortingAndSearching/sortAnArray.py
@@ -0,0 +1,17 @@
+
+def sortArray(nums):
+    n = len(nums)
+
+    for i in range(n):
+        isSwap = False
+        for j in range(n-i-1):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+                isSwap = True
+
+        if not isSwap:
+            break
+
+    return nums
