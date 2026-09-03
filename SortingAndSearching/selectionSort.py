40506d191828cd01987b2de747839e81d73d6eab
Prashantpd7 <prashantdwivedipd7@gmail.com>
2026-09-03 19:45:39 +0530

    sorting

---
+++ SortingAndSearching/selectionSort.py
@@ -0,0 +1,15 @@
+def selection(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min = i
+
+        for j in range(i+1, n):
+            if arr[j]<arr[min]:
+                min = j
+
+        arr[i], arr[min] = arr[min], arr[i]
+
+    return arr
+
+print(selection([5,3,8,6,1,9]))
